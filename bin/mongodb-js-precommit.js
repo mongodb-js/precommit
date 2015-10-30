e6ccdbe4aac174d75dea443a3fa22fbf5efcffce
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-30 12:46:51 -0400

    :tada: precommit now as fast and helpful as fmt

--- bin/mongodb-js-precommit.js
+++ bin/mongodb-js-precommit.js
@@ -1,9 +1,47 @@
 #!/usr/bin/env node
 
-require('../')(function(err) {
+/* eslint no-sync:0, no-console:0 */
+var path = require('path');
+var fs = require('fs');
+var chalk = require('chalk');
+var figures = require('figures');
+
+var usage = fs.readFileSync(path.resolve(__dirname, '../usage.txt')).toString();
+var args = require('minimist')(process.argv.slice(2), {
+  boolean: ['debug', 'json']
+});
+
+if (args.debug) {
+  process.env.DEBUG = 'mongodb-js-precommit';
+}
+var precommit = require('../');
+var pkg = require('../package.json');
+
+args.globs = args._;
+
+if (args.help || args.h) {
+  console.error(usage);
+  process.exit(1);
+}
+if (args.version) {
+  console.error(pkg.version);
+  process.exit(1);
+}
+
+precommit(args, function(err, res) {
   if (err) {
-    console.error(err);
-    return process.exit(1);
+    if (args.json) {
+      err = JSON.stringify(err, null, 2);
+    }
+    console.error(chalk.red(figures.cross), err.message);
+    console.error(chalk.gray(err.stack));
+    process.exit(1);
+    return;
   }
-  console.log('ok');
+
+  if (args.json) {
+    console.log(JSON.stringify(res, null, 2));
+  } else {
+    console.log('\n\n', chalk.green(figures.tick), ' ok!  no potential errors found');
+  }
 });
--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-var dc = require('dependency-check');
+/* eslint no-console:0 */
 var format = require('util').format;
-var debug = require('debug')('mongodb-js-precommit');
+var dc = require('dependency-check');
 var glob = require('glob');
+var defaults = require('lodash.defaults');
+var unique = require('lodash.uniq');
 var CLIEngine = require('eslint').CLIEngine;
 var async = require('async');
+var path = require('path');
+var chalk = require('chalk');
+var figures = require('figures');
+var assign = require('lodash.assign');
+var debug = require('debug')('mongodb-js-precommit');
+
+/**
+ * Expand globs into paths.
+ */
+function resolve(opts, done) {
+  debug('resolving paths for globs:\n', JSON.stringify(opts.globs));
+  var tasks = opts.globs.map(function(pattern) {
+    return function(cb) {
+      debug('resolving `%s`...', pattern);
+      glob(pattern, {}, function(err, files) {
+        if (err) {
+          return cb(err);
+        }
+        debug('resolved %d file(s) for `%s`', files.length, pattern);
+        if (files.length > 0) {
+          opts.files.push.apply(opts.files, files);
+        }
+        cb();
+      });
+    };
+  });
+  async.parallel(tasks, function(err) {
+    if (err) {
+      return done(err);
+    }
+    debug('checking and removing duplicate paths...');
+    opts.files = unique(opts.files);
+    debug('final result has `%d` files', opts.files.length);
+    done(null, opts.files);
+  });
+}
+
 
 function check(options, mode, done) {
-  var pkg = require(process.cwd() + '/package.json');
-  pkg['dependency-check'] = pkg['dependency-check'] || {
-      entries: [],
-      ignore: []
-    };
+  if (mode === 'extra') {
+    console.log('  ' + chalk.gray(figures.pointerSmall,
+        'Checking for dependencies in package.json not used in code'
+        + figures.ellipsis));
+  } else {
+    console.log('  ' + chalk.gray(figures.pointerSmall,
+        'Checking for dependencies used in code but not added to package.json'
+        + figures.ellipsis));
+  }
+
+  var pkg = require(path.join(options.dir, 'package.json'));
+  defaults(pkg['dependency-check'], {
+    entries: [],
+    ignore: []
+  });
   pkg.devDependencies = pkg.devDependencies || {};
 
   var opts = {
-    path: process.cwd() + '/package.json',
+    path: path.join(options.dir, 'package.json'),
     entries: pkg['dependency-check'].entries,
     ignore: pkg['dependency-check'].ignore
   };
@@ -45,8 +94,11 @@ function check(options, mode, done) {
   }
 
   dc(opts, function(err, data) {
-    if (err) return done(err);
-    var pkg = data.package;
+    if (err) {
+      return done(err);
+    }
+    pkg = data.package;
+
     var deps = data.used;
     var results;
     var errMsg;
@@ -67,9 +119,19 @@ function check(options, mode, done) {
     }
 
     if (results.length === 0) {
+      if (mode === 'extra') {
+        console.log('  ' + chalk.green(figures.tick),
+          ' No extra dependencies in package.json');
+      } else {
+        console.log('  ' + chalk.green(figures.tick),
+          ' All dependencies declared in package.json');
+      }
       return done();
     }
-    console.error('Error: ' + errMsg + '. To fix this, run:\n\n    ' + corrector + '\n');
+    errMsg += '\n' + corrector;
+    errMsg += '\nPlease see the configuration docs for more info:\n';
+    errMsg += 'https://github.com/mongodb-js/precommit#configuration';
+
     options.result.errors.push(new Error(errMsg));
     return done();
   });
@@ -80,59 +142,93 @@ var lint = function(opts, done) {
     useEslintrc: true
   });
 
-  debug('Getting paths to lint...');
-  // @todo (imlucas): See `mongodb-js-fmt` --changed option.
-  // Should use the solution for that here as well by default
-  // bc we're a precommit hook and should only be looking
-  // at the files the user actually changed.
-  debug('Linting files', opts.files);
+  /**
+   * @todo (imlucas): See `mongodb-js-fmt` --changed option.
+   * Should use the solution for that here as well by default
+   * bc we're a precommit hook and should only be looking
+   * at the files the user actually changed.
+   */
+  debug('linting files', opts.files);
+  console.log('  ' + chalk.gray(figures.pointerSmall,
+      format('Running eslint on %d files%s', opts.files.length,
+        figures.ellipsis)));
+
   var report = cli.executeOnFiles(opts.files);
   var formatter = cli.getFormatter();
-  console.log(formatter(report.results));
+  if (!opts.json) {
+    opts.result.eslint = formatter(report.results);
+  } else {
+    opts.result.eslint = report.results;
+  }
 
+  debug('eslint result', JSON.stringify(report.results, null, 2));
   if (report.errorCount > 0) {
+    console.log(opts.result.eslint);
     var err = new Error(format(
-      'Please fix the %d error(s) above and try again.', report.errorCount));
+      'Please fix the %d error(s) above and try again.',
+      report.errorCount));
     opts.result.errors.push(err);
-    return done();
+  } else {
+    console.log('  ' + chalk.green(figures.tick),
+      ' No errors found by eslint');
   }
   done();
 };
 
-module.exports = function(done) {
-  var pkg = require(process.cwd() + '/package.json');
-  var opts = {
-    ignore: [
-      'node_modules/**'
-    ]
-  };
-  pkg.check = pkg.check || {};
-  pkg.check.ignore = pkg.check.ignore || [];
+module.exports = function(opts, done) {
+  defaults(opts, {
+    dir: process.cwd(),
+    files: [],
+    dry: false,
+    json: false,
+    formatted: [],
+    unchanged: [],
+    result: {
+      errors: []
+    }
+  });
 
-  opts.ignore.push.apply(opts.ignore, pkg.check.ignore);
-  opts.result = {
-    errors: []
-  };
+  if (opts.globs.length === 0) {
+    // @todo (imlucas): yeah, I can never remember how
+    // to properly exclude node_modules either...
+    opts.globs = [
+      './bin/*.js',
+      './lib/{**/*,*}.js',
+      './examples/{**/*,*}.js',
+      './src/{**/*,*}.js',
+      './test/{**/*,*}.js',
+      './*.js'
+    ];
+  }
 
-  glob('**/*.js', opts, function(err, files) {
-    if (err) return done(err);
-    debug('project has %d files', files.length);
+  if (!Array.isArray(opts.globs)) {
+    opts.globs = [opts.globs];
+  }
 
-    opts.files = files;
-    async.series({
-      'missing dependencies': check.bind(null, opts, 'missing'),
-      'extra dependencies': check.bind(null, opts, 'extra'),
-      lint: lint.bind(null, opts)
-    }, function(err) {
-      if (err) {
-        return done(err);
-      }
-      if (opts.result.errors.length > 0) {
-        var error = new Error(format('%d check(s) failed', opts.result.errors.length));
-        error.errors = opts.result.errors;
-        return done(error);
-      }
-      return done();
-    });
+  console.log(chalk.gray(
+    'Checking for potential errors' + figures.ellipsis));
+  console.log(chalk.gray('Use the'),
+    chalk.gray.bold('--debug'),
+    chalk.gray('flag to print diagnostic info')
+  );
+
+  console.log(chalk.gray('For more info, please see'),
+    chalk.gray.bold('https://github.com/mongodb-js/precommit'), '\n');
+
+  async.series([
+    resolve.bind(null, opts),
+    check.bind(null, opts, 'missing'),
+    check.bind(null, opts, 'extra'),
+    lint.bind(null, opts)
+  ], function(err) {
+    if (err) {
+      return done(err);
+    }
+    if (opts.result.errors.length > 0) {
+      var error = new Error(format('%d check(s) failed', opts.result.errors.length));
+      assign(error, opts);
+      return done(error);
+    }
+    return done(null, opts);
   });
 };
